c7b647aab777510df957f0e7b639492556f29831
jackeline <jackeline@JACKELINEP>
2014-10-20 22:20:32 +0200

    Creacion del patron Flyweight en la clase FactoriaCaracter

--- src/main/java/es/upm/miw/pd/patrones/text/FactoriaCaracter.java
+++ src/main/java/es/upm/miw/pd/patrones/text/FactoriaCaracter.java
@@ -1,13 +1,24 @@
 package es.upm.miw.pd.patrones.text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FactoriaCaracter {
 	private static FactoriaCaracter factory;
-	
+	private  Map<Character, Caracter> map = new HashMap<Character, Caracter>();
+
 	private FactoriaCaracter(){		
 	}
 		
-	public Caracter get(char caracter) {			
-		return null;
+	public Caracter get(char key) {			
+		if(map.containsKey(key)){
+			return map.get(key);
+		}else{
+			//Construccion perezosa
+			Caracter pl = new Caracter(key);
+			map.put(key, pl);
+			return pl;
+		}
 	}
 	
 	public static FactoriaCaracter getFactoria() {
